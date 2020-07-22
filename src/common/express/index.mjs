18a78bbb1fd3f7a9548200764c578e0f18cd746a
yzhao <295881455@qq.com>
2020-07-22 17:38:09 +0800

    add express

---
+++ src/common/express/index.mjs
@@ -0,0 +1,21 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+export default function init() {
+  const app = express();
+  app.use(bodyParser.json());
+
+  app.get('/', (req, res) => {
+    console.log(req.body);
+    const data = req.body;
+    if (data['post_type'] === 'message' && data['message_type'] === 'group' && data['sub_type'] !== 'notice') {
+      const rawMessage = data['raw_message'];
+      console.log(rawMessage);
+    }
+    res.status(204).json({});
+  });
+
+  app.listen(3000, () => {
+    console.log('express is listening on port 3000');
+  });
+}
--- src/index.mjs
+++ src/index.mjs
@@ -1,12 +1,19 @@
 import {install} from './common/sqlite/init.mjs';
 import {getHeroes, getItems} from './common/steam/init.mjs';
-import {getFormatHeroInfo} from './common/huiji/hero';
+import {getFormatHeroInfo} from './common/huiji/hero.mjs';
 
-async function run() {
+async function init() {
   try {
     await install();
     await getHeroes();
     await getItems();
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+async function run() {
+  try {
     const info = await getFormatHeroInfo(3);
     console.log(info);
   } catch (e) {
