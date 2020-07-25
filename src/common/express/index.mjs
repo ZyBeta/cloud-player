7f44df8df00867f6bd407bbcc8a86656119f496c
ZyBeta <295881455@qq.com>
2020-07-25 09:29:02 +0800

    add error handler

--- src/common/express/index.mjs
+++ src/common/express/index.mjs
@@ -18,100 +18,104 @@ export default async function init() {
 
     app.post('/', async (req, res) => {
         const data = req.body
-        if (data.post_type === 'message' && data.sub_type !== 'notice') {
-            if (data.message_type === 'private' && CONFIG.private_message
+        try {
+            if (data.post_type === 'message' && data.sub_type !== 'notice') {
+                if (data.message_type === 'private' && CONFIG.private_message
                 || data.message_type === 'group' && CONFIG.group_message
                 || data.message_type === 'discuss' && CONFIG.discuss_message) {
-                const rawMessage = data.raw_message
-                const parsedCommand = parseCommand(rawMessage)
-                if (parsedCommand) {
-                    if (parsedCommand.key === 'help') {
-                        await sendMessage({
-                            ...data,
-                            message: COMMAND_TEXT,
-                        })
-                    }
-                    if (parsedCommand.key === 'query') {
-                        const target = parsedCommand.params.t
-                        const query = parsedCommand.params.q
-                        if (target && query) {
-                            if (target.indexOf('hero') !== -1) {
-                                const hero = await getHeroByName(query[0])
-                                if (hero) {
-                                    const result = await getFormatHeroInfo(hero.id)
-                                    await sendMessage({
-                                        ...data,
-                                        message: result,
-                                    })
-                                }
-                            }
-                            if (target.indexOf('item') !== -1) {
-                                const item = await getItemByName(query[0])
-                                if (item) {
-                                    const result = await getFormatItemInfo(item.id)
-                                    await sendMessage({
-                                        ...data,
-                                        message: result,
-                                    })
-                                }
-                            }
-                        } else {
+                    const rawMessage = data.raw_message
+                    const parsedCommand = parseCommand(rawMessage)
+                    if (parsedCommand) {
+                        if (parsedCommand.key === 'help') {
                             await sendMessage({
                                 ...data,
-                                message: `missing parameter(s)`,
+                                message: COMMAND_TEXT,
                             })
                         }
-                    }
-                    if (parsedCommand.key === 'random') {
-                        const target = parsedCommand.params.t
-                        if (target) {
-                            if (target.indexOf('hero') !== -1) {
-                                const hero = await getHeroRandom()
-                                if (hero) {
-                                    const result = await getFormatHeroInfo(hero.id)
-                                    await sendMessage({
-                                        ...data,
-                                        message: result,
-                                    })
+                        if (parsedCommand.key === 'query') {
+                            const target = parsedCommand.params.t
+                            const query = parsedCommand.params.q
+                            if (target && query) {
+                                if (target.indexOf('hero') !== -1) {
+                                    const hero = await getHeroByName(query[0])
+                                    if (hero) {
+                                        const result = await getFormatHeroInfo(hero.id)
+                                        await sendMessage({
+                                            ...data,
+                                            message: result,
+                                        })
+                                    }
                                 }
-                            }
-                            if (target.indexOf('item') !== -1) {
-                                const item = await getItemRandom()
-                                if (item) {
-                                    const result = await getFormatItemInfo(item.id)
-                                    await sendMessage({
-                                        ...data,
-                                        message: result,
-                                    })
+                                if (target.indexOf('item') !== -1) {
+                                    const item = await getItemByName(query[0])
+                                    if (item) {
+                                        const result = await getFormatItemInfo(item.id)
+                                        await sendMessage({
+                                            ...data,
+                                            message: result,
+                                        })
+                                    }
                                 }
+                            } else {
+                                await sendMessage({
+                                    ...data,
+                                    message: `missing parameter(s)`,
+                                })
                             }
-                        } else {
-                            await sendMessage({
-                                ...data,
-                                message: `missing parameter(s)`,
-                            })
                         }
-                    }
-                } else if (!CONFIG.only_command) {
-                    let find = cachedHeroes.find((hero) => rawMessage.indexOf(hero.name_zh) !== -1)
-                    if (find) {
-                        const result = await getFormatHeroInfo(find.id)
-                        await sendMessage({
-                            ...data,
-                            message: result,
-                        })
-                    } else {
-                        find = cachedItems.find((item) => rawMessage.indexOf(item.name_zh) !== -1)
+                        if (parsedCommand.key === 'random') {
+                            const target = parsedCommand.params.t
+                            if (target) {
+                                if (target.indexOf('hero') !== -1) {
+                                    const hero = await getHeroRandom()
+                                    if (hero) {
+                                        const result = await getFormatHeroInfo(hero.id)
+                                        await sendMessage({
+                                            ...data,
+                                            message: result,
+                                        })
+                                    }
+                                }
+                                if (target.indexOf('item') !== -1) {
+                                    const item = await getItemRandom()
+                                    if (item) {
+                                        const result = await getFormatItemInfo(item.id)
+                                        await sendMessage({
+                                            ...data,
+                                            message: result,
+                                        })
+                                    }
+                                }
+                            } else {
+                                await sendMessage({
+                                    ...data,
+                                    message: `missing parameter(s)`,
+                                })
+                            }
+                        }
+                    } else if (!CONFIG.only_command) {
+                        let find = cachedHeroes.find((hero) => rawMessage.indexOf(hero.name_zh) !== -1)
                         if (find) {
-                            const result = await getFormatItemInfo(find.id)
+                            const result = await getFormatHeroInfo(find.id)
                             await sendMessage({
                                 ...data,
                                 message: result,
                             })
+                        } else {
+                            find = cachedItems.find((item) => rawMessage.indexOf(item.name_zh) !== -1)
+                            if (find) {
+                                const result = await getFormatItemInfo(find.id)
+                                await sendMessage({
+                                    ...data,
+                                    message: result,
+                                })
+                            }
                         }
                     }
                 }
             }
+        } catch (e) {
+            consola.error(e)
         }
         await res.status(204).json({})
     })
